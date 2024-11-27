c48613aeb635e5e9db51b01a6b06a323e3db570e
Agedis <houuinkyouma@gmail.com>
2024-11-27 16:15:01 -0500

    Implemented editStudy method in the database backend. Changed CommonStudy to take in String username as a input rather than user object (this is needed if we want getStudies to return actual CommonStudy objects rather than Map containing information of the study)

--- src/main/java/use_case/view_experiment/ViewExperimentDataAccessInterface.java
+++ src/main/java/use_case/view_experiment/ViewExperimentDataAccessInterface.java
@@ -1,7 +1,8 @@
 package use_case.view_experiment;
 
 import entity.CommonStudy;
+import org.json.JSONArray;
 
 public interface ViewExperimentDataAccessInterface {
-    CommonStudy getResearchStudy(String id);
+    JSONArray getStudies ();
 }
